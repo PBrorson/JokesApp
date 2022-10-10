6078581beb41e4b1a7bc29dd526d0ba38ed1e02b
PBrorson <patrikbrorson@hotmail.com>
2022-10-10 14:56:13 +0200

    Add project files.

---
+++ JokesLibrary/Data/JokesContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using JokesLibrary.Models;
+
+    public class JokesContext : DbContext
+    {
+        public JokesContext (DbContextOptions<JokesContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<JokesLibrary.Models.Joke> Joke { get; set; } = default!;
+    }
---
+++ JokesLibrary/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JokesLibrary.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
